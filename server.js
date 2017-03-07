85083f9a8a5db20997c456c74eb38143fd2144d6
João Victor de Lemos Nery <joao.victor.lemos.nery@gmail.com>
2017-03-07 18:29:59 -0300

    teste

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var app = express();
 
 // set the port of our application
 // process.env.PORT lets the port be set by Heroku
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 3000;
 
 // set the view engine to ejs
 app.set('view engine', 'ejs');
